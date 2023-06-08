1145ca6941afb0dbe2fbd14e53907cddb88587b7
Wahome <vinwahome@gmail.com>
2023-06-08 10:12:46 +0300

    Make routes for finding individual objects

--- G.O.T-server/app/controllers/application_controller.rb
+++ G.O.T-server/app/controllers/application_controller.rb
@@ -1,14 +1,32 @@
 class ApplicationController < Sinatra::Base
     set default_content_type: "application/json"
 
+    #houses routes
     get '/houses' do 
         house = House.all
         house.to_json
     end
 
+    get '/houses/:id' do
+        house = House.find(params[:id])
+        house.to_json
+    end
+
+    # get '/houses/:name' do
+    #     house = House.find_by(name: params[:name])
+    #     house.to_json
+    # end
+
+
+    #character routes
     get '/characters' do 
         characters = Character.all
         characters.to_json
+    end
+
+    get '/characters/:id' do
+        character = Character.find(params[:id])
+        character.to_json
     end
     
 end
